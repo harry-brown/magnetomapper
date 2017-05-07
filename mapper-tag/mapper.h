2a34401eaad2286aabaa336387ff14c28b0588e0
harry-brown <hjb.harrybrown@gmail.com>
2017-05-08 00:10:47 +1000

    attempting to interface magnetometer i2c write is failing

--- mapper-tag/mapper.h
+++ mapper-tag/mapper.h
@@ -7,21 +7,28 @@
  * \author
  *         Harry Brown
  */
-
-
+/*---------------------------------------------------------------------------*/
+#ifndef MAPPER_H_
+#define MAPPER_H_
 /* Includes ------------------------------------------------------------------*/
+#include "mpu-9250-sensor.h"
 #include "contiki.h"
 #include "dev/serial-line.h"
 #include "dev/cc26xx-uart.h"
 #include "dev/leds.h"
 #include "sys/ctimer.h"
+#include "sensor-common.h"
+#include "lib/sensors.h"
 
 #include <stdio.h>
 #include <string.h>
-/*----------------------------------------------------------------------------*/
-
 
 /* Function Prototypes -------------------------------------------------------*/
 static void init(void);
 static void alive_timeout(void *ptr);
-/*----------------------------------------------------------------------------*/
+static void init_mpu_reading(void *not_used);
+static void get_mpu_reading();
+static void print_mpu_reading(int reading);
+
+/*---------------------------------------------------------------------------*/
+#endif /* MAPPER_H_ */
